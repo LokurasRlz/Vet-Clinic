0a7cd2aab268559d5a580949a593a24835f50ed3
Matias Aguirre <aguirremati09@gmail.com>
2022-10-17 19:05:34 -0300

    Set up schema, queries and data

--- schema.sql
+++ schema.sql
@@ -1,15 +1,13 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE public."animals "
+CREATE TABLE animals
 (
     id integer NOT NULL,
-    "name " character varying,
+    "name"  character varying,
     date_of_birth date,
     escape_attempts integer,
     neutered boolean,
-    weight_kg numeric,
+    weight_kg decimal,
     PRIMARY KEY (id)
 );
 
-ALTER TABLE IF EXISTS public."animals "
-    OWNER to postgres;
